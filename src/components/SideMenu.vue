8453de449b053ba0b8617255b7a8b75a99ff7b62
Yoshiya Noda <ny1425.0106@gmail.com>
2021-01-29 20:23:42 +0900

    サイドバーのボタン変えた

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -3,6 +3,9 @@
     <div class="linkContainer">
       <ul>
         <li>
+          <router-link to="/login">Sign up</router-link>
+        </li>
+        <li>
           <router-link to="/">Home</router-link>
         </li>
         <li>
@@ -10,6 +13,9 @@
         </li>
         <li>
           <router-link to="/about">About</router-link>
+        </li>
+        <li>
+          <router-link to="/create">Create Web Site</router-link>
         </li>
       </ul>
     </div>
@@ -24,25 +30,28 @@ export default {
 
 <style scoped lang="scss">
 #sidemenu {
-  width: 17%;
-  background-color: rgba(100,100,130,.1);
+  width: 15%;
+  background-color: rgba(200,200,230,.1);
 }
 .linkContainer {
   padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    text-decoration-line: none;
-
-    &.router-link-exact-active {
-      color: gray;
-    }
-  }
   > ul {
+    padding: 5px;
     list-style-type: none;
     > li {
       padding: 5px;
+      a {
+        font-weight: bold;
+        color: #2c3e50;
+        text-decoration-line: none;
+        font-size: 0.8rem;
+
+        &.router-link-exact-active {
+          color: gray;
+        }
+      }
+
     }
   }
 }
