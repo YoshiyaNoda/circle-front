277465fbc9f8bea4e6750b8219134e30563864c9
Yoshiya Noda <ny1425.0106@gmail.com>
2021-02-08 12:28:15 +0900

    記事一覧を取得する処理を書いた。

--- src/views/SelectArticle.vue
+++ src/views/SelectArticle.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="wrapper">
     <p>記事を選択、あるいは作成してください。</p>
+    <div class="articlelist-container">
+      <ul>
+        <li v-for="article in articleList" :key="article.id">{{ article.title }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      'articleList': []
+    };
+  },
+  mounted() {
+    
+  },
+  methods: {
+    async fetchArticleList() {
+      const url = "fetch-article-list";
+      const params = new URLSearchParams();
+      await this.$axios.post(url, params).then(res => {
+        this.articleList = res.data;
+      });
+    }
+  }
 }
 </script>
 
@@ -14,6 +35,7 @@ export default {
 .wrapper {
   width: 100%;
   height: 100%;
+  padding: 30px 60px;
   background-color: white;
 }
 </style>
