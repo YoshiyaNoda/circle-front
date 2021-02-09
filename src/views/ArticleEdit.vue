eacec09cb55e615d9f1244760d42d1017e794e74
Yoshiya Noda <ny1425.0106@gmail.com>
2021-02-09 10:24:20 +0900

    記事のデータの取得画面の雛形を作成

--- src/views/ArticleEdit.vue
+++ src/views/ArticleEdit.vue
@@ -1,12 +1,38 @@
 <template>
   <div class="article-edit-wrapper">
     <h1>記事の編集</h1>
+    <div class="main-editor-container">
+      <div class="editor" v-for="json in jsonData" :key="json.order">{{ json.name }}</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      jsonData: [],
+      title: '',
+      url: '',
+    }
+  },
+  mounted() {
 
+  },
+  methods: {
+    async fetchArticleData() {
+      const url = "fetch-article-data"
+      const params = new URLSearchParams()
+      await this.$axios.post(url, params).then(res => {
+        this.json = res.data.json
+        this.title = res.data.title
+        this.url = res.data.url
+      }).catch(e => {
+        console.log(e)
+        alert("データの取得に失敗しました")
+      })
+    }
+  }
 }
 </script>
 
