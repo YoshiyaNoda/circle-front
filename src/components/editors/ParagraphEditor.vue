cbc7fc9fa57cfc0866e7ceb0144561895b2c2442
Yoshiya Noda <ny1425.0106@gmail.com>
2021-02-13 05:15:28 +0900

    contenteditableの検証とデバッグ用のtoken設定

--- src/components/editors/ParagraphEditor.vue
+++ src/components/editors/ParagraphEditor.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <div class="paragraphEditor">
+      <div v-text="d.data.content" contenteditable="true" @input="sync"></div>
+    </div>
     <textarea type="text" v-model="d.data.content"></textarea>
   </div>
 </template>
@@ -14,7 +17,14 @@ export default {
   data: function() {
     return {
       d: this.articleData
-    };
+    }
+  },
+  methods: {
+    sync(e) {
+      this.$emit('update', e.target.innerHTML)
+      console.log(e.target.innerHTML)
+      this.d.data.content = e.target.innerHTML
+    }
   },
   watch: {
     articleData: function(data) {
--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ Vue.prototype.$axios = axios.create({
 });
 
 const store = {
-  token: '',
+  token: 'ya29.a0AfH6SMBMynDVhDXaeauqakgHX7CgtJP8rjJCGhOuQueTyu0bKUV3Qv4rL0WSCYDlpp370yC7tBof6EHQPk2znvCvUWYrVU0ymIJprG9Zv_ZN5col988R43yhJpmggF7eVoigJgHD584uXBeSYayF_xveQpUsO5dxSxHJGFxL2JU', //デバッグ用
   email: '',
   name: '',
   selectedArticleId: 0,
