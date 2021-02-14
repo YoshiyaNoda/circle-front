166f04ad200496ac6de960c184a919ca94b59c01
Yoshiya Noda <ny1425.0106@gmail.com>
2021-02-15 08:36:56 +0900

    画像選択コンポーネントの作成

---
+++ src/components/editors/ImageSelector.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="imageSelectorContainer">
+    <p>hoge</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.imageSelectorContainer {
+  position: fixed;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 20%;
+  border: solid 1px rgba(0,0,0,.2);
+  border-right: none;
+  background-color: rgba(0,0,0,.6);
+}
+</style>
