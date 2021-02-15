361947aeb50502eba2614764525e23ee226ebc6b
Yoshiya Noda <ny1425.0106@gmail.com>
2021-02-15 15:50:14 +0900

    画像をfetchできるようになった

--- src/components/editors/ImageSelector.vue
+++ src/components/editors/ImageSelector.vue
@@ -4,12 +4,38 @@
       <button @click="emitClick">アップロード</button>
       <input type="file" @change="upload" name="image" ref="imageForm"  accept="image/*" >
     </div>
+    <div class="imageListArea">
+      <div class="image" v-for="(image, idx) in imageList" :key="'image'+idx">
+        <img :src="image.read_path">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  mounted() {
+    this.fetchImageList()
+  },
+  data() {
+    return {
+      imageList: []
+    }
+  },
   methods: {
+    async fetchImageList() {
+      const url = "fetch-image-list"
+      const params = new URLSearchParams()
+      params.append('token', this.$store.token)
+      await this.$axios.post(url, params).then(res => {
+        this.syncImageList(res.data)
+      }).catch(e => {
+        console.log(e)
+      })
+    },
+    syncImageList(imageList) {
+      this.imageList = imageList
+    },
     async upload(e) {
       const file = e.target.files[0]
       const url = "upload-image"
@@ -17,7 +43,13 @@ export default {
       params.append('image', file)
       params.append('token', this.$store.token)
       await this.$axios.post(url, params).then(res => {
-        console.log(res.data)
+        if(res.data === "over") {
+          alert("画像の登録数の上限を超えています。")
+        } else if(res.data === "fail") {
+          alert('この画像はアップロードできません。')
+        } else {
+          this.syncImageList(res.data)
+        }
       }).catch(e => {
         console.log(e)
         alert('この画像はアップロードできません。')
@@ -42,6 +74,15 @@ export default {
   background-color:rgb(240, 240, 240);
   z-index: 200;
   padding: 5px;
+  > .imageListArea {
+    width: 100%;
+    > .image {
+      width: 100%;
+      > img {
+        width: 100%;
+      }
+    }
+  }
   > .uploadArea {
     width:80%;
     height:100px;
